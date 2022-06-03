185d1c684a86ab63e911bad1228a6634bfb5c144
KYANG <907480792@qq.com>
2022-06-03 11:01:33 +0800

    test calculator

---
+++ src/java/CalculatorTest.java
@@ -0,0 +1,11 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CalculatorTest {
+  @Test
+    public void shouldreturntow(){
+    Assert.assertEquals(0,0);
+  }
+}
